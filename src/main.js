43072c22d9154ebe71a62b2095ac971f60e3a836
kirianguiller <kirianguiller@gmail.com>
2021-05-05 08:28:20 +0200

    remove minimalDepTree component

--- src/main.js
+++ src/main.js
@@ -6,10 +6,9 @@ Vue.config.productionTip = false;
 Vue.use(vueCustomElement);
 
 import ReactiveDepTree from "./ReactiveDepTree.vue";
-import MinimalDepTree from "./MinimalDepTree.vue";
 
 // Configure Vue to ignore the element name when defined outside of Vue.
-Vue.config.ignoredElements = ["reactive-dep-tree", "minimal-dep-tree"];
+Vue.config.ignoredElements = ["reactive-dep-tree"];
 
 // Enable the plugin
 
@@ -26,21 +25,5 @@ Vue.customElement("reactive-dep-tree", ReactiveDepTree, {
       root.shadowRoot = document.head;
     }
     return root;
-  }
-});
-
-// Register your component
-Vue.customElement("minimal-dep-tree", MinimalDepTree, {
-  // Additional Options: https://github.com/karol-f/vue-custom-element#options
-  shadow: true,
-  beforeCreateVueInstance(root) {
-    const rootNode = root.el.getRootNode();
-
-    if (rootNode instanceof ShadowRoot) {
-      root.shadowRoot = rootNode;
-    } else {
-      root.shadowRoot = document.head;
-    }
-    return root;
-  }
+  },
 });
